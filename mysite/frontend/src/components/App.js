d6ca5d21be8cc81e53d3e1e9d1bb4344c2a4256c
felipe <felipe.heyman@edu.pucrs.br>
2019-11-18 16:31:34 -0300

    redux

--- mysite/frontend/src/components/App.js
+++ mysite/frontend/src/components/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './layout/header'
-class App extends React.Component{
-    render(){
-        return (
-            <Header />
-        )
-    }
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Header from "./layout/header";
+
+import { Provider } from "react-redux";
+import store from "../store";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Header />;
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
